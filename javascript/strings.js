b7384301a49e4d810d0aced6fb67e06998da8317
Akshat Tewari <tewariakshat50@gmail.com>
2025-10-17 22:40:53 +0530

    adding variables and data types lesson

---
+++ javascript/strings.js
@@ -0,0 +1,18 @@
+//STRINGS
+//In JavaScript, you can choose single quotes ('), double quotes ("), or backticks (`) to wrap your strings in.
+ const name = "Chris";
+ const greeting = `Hello, ${name}`;
+ console.log(greeting); // "Hello, Chris"
+ const joined = `${greeting} ${string}`;
+ console.log(joined);
+let text="HELLO WORLD";
+ console.log(text.charAt(4));
+let letter=text.at(2);
+console.log(letter);
+//Strings are immutable: Strings cannot be changed, only replaced.
+//All string methods return a new string. They don't modify the original string
+console.log(text.slice(2,4));
+console.log(text.slice(-5,-1));
+console.log(text.slice(2));
+
+
---
+++ javascript/variables.js
@@ -0,0 +1,21 @@
+let a = "Hello";
+console.log(a);
+a = 9;
+console.log(a);
+const b = "World";
+console.log(b);
+// b = 10; // This will cause an error because b is a constant and cannot be reassigned.
+console.log((3 + 2) - 76 * (1 + 1));
+const max=57;
+const actual=max-15;
+const percentage=actual/max;
+console.log(percentage*100)
+// We can declare variables to store data by using the var, let, or const keywords.
+// let – is a modern variable declaration.
+// var – is an old-school variable declaration. Normally we don’t use it at all, 
+// but we’ll cover subtle differences from let in the chapter 
+// The old "var", just in case you need them.
+// const – is like let, but the value of the variable can’t be changed.
+// Variables should be named in a way that allows us to easily understand what’s inside them.
+
+
