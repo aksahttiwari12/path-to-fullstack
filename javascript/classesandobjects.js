238645aeacf1705ce4ff8a0ce726b94b5b7bbeb2
Akshat Tewari <tewariakshat50@gmail.com>
2026-07-09 01:42:15 +0530

    learning classes and objects

--- javascript/classesandobjects.js
+++ javascript/classesandobjects.js
@@ -3,7 +3,7 @@
 //
 
 class Rectangle {
-    Constructor(height,width,color){
+    constructor(height,width,color){
         this.height = height; // its saying like the width of this this object(say rect) is equal to the height variable we wrote in class
         this.width  = width;
         this.color = color;
@@ -23,3 +23,19 @@ class Rectangle {
 
 //the code above is representing a class rectangle.What we have done here is that we have prepared a structure that we are going to use again and again to build something
 //In this case we are building objects with the use of classes. (Now these objects are different from the other object that we studied in javascript)
+
+//forming objects:
+const rect = new Rectangle(4,5,"red");
+const area = rect.area();
+//the . here basically connects the object with the properties or methods
+console.log(area);
+
+//Some important  predefined classes
+const now = new Date(); //Current date and time
+console.log(now.toISOString());
+ //map class
+
+ const map = new map();
+ map.set('name' , 'Alice');
+ map.set('Age' , '20');
+ console.log(map.get('name'));
